39a940a1a2a52995a5ded0b44c6a65bf3ca150f9
Emrecan Altinsoy <emrecan.altinsoy@se.com>
2024-12-02 15:54:31 +0100

    Update astrocommunity packages

--- dot_config/nvim/lua/community.lua
+++ dot_config/nvim/lua/community.lua
@@ -5,9 +5,26 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  -- Packs
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.vue" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.chezmoi" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.terraform" },
+  { import = "astrocommunity.pack.yaml" },
+  -- Indentation
   { import = "astrocommunity.indent.indent-rainbowline" },
+  -- Diagnostics
   { import = "astrocommunity.diagnostics.trouble-nvim" },
+  -- Animations
+  { import = "astrocommunity.scrolling.mini-animate" },
+  -- Utility
+  { import = "astrocommunity.utility.hover-nvim" },
   -- import/override with your plugins folder
 }
